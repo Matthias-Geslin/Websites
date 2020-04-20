858f6c6a37b11f6f5118e483e92f51a23fa3b920
Matthias <m.geslin974@gmail.com>
2020-04-20 18:42:24 +0200

    added methods from crud

--- src/Controller/ProgressController.php
+++ src/Controller/ProgressController.php
@@ -22,10 +22,69 @@ class ProgressController extends MainController
      */
     public function launchMethod()
     {
-        // $progress = ModelMaker::getModel('Progress')->listData();
+        $progress = ModelMaker::getModel('Progress')->listData();
 
         return $this->render('progress.twig',[
-            // 'progress'  => $progress
+            'progress'  => $progress
         ]);
+    }
+
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function createMethod()
+    {
+      if (!empty($this->getFileVar('name'))) {
+           $file = $this->uploadFile('img/Progress');
+       }
+
+       if (empty($this->post)) {
+           return $this->render('backend/progressCreate.twig');
+       }
+       ModelMaker::getModel('Progress')->createData([
+           'name'        => $this->post['name'],
+           'file'        => $file,
+           'link'        => $this->post['link'],
+           'year'        => $this->post['year'],
+           'description' => $this->post['description'],
+           'category'    => $this->post['category']
+       ]);
+       $this->redirect('user');
+    }
+
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function deleteMethod()
+    {
+        ModelMaker::getModel('Progress')->deleteData($this->get['id']);
+
+        $this->redirect('user');
+    }
+
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function modifyMethod()
+    {
+        if (!empty($this->post)) {
+            $this->uploadingFile('Progress');
+        }
+        if ($this->getUserVar('status') == 'Admin') {
+            $progress = ModelMaker::getModel('Progress')->readData($this->get['id']);
+
+            return $this->render('backend/progressModify.twig', [
+                'progress' => $progress
+            ]);
+        } $this->redirect('home');
     }
 }
