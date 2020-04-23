b7ec282233970938e9bf35abc8ab202b6e75a866
Matthias <m.geslin974@gmail.com>
2020-04-23 16:15:40 +0200

    added methods to use crud

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -14,6 +14,11 @@ use Twig\Error\SyntaxError;
 class BlogController extends MainController
 {
     /**
+     * @return array
+     */
+    private $post_content = [];
+
+    /**
      * Renders the View Blog
      * @return string
      * @throws LoaderError
@@ -22,10 +27,82 @@ class BlogController extends MainController
      */
     public function launchMethod()
     {
-        // $blog = ModelMaker::getModel('Blog')->listData();
+        $blog = ModelMaker::getModel('Blog')->listData();
 
         return $this->render('blog.twig',[
-            // 'blog'  => $blog
+            'blog'  => $blog
         ]);
+    }
+
+    /**
+     * @return string
+     */
+    private function postData()
+    {
+      $this->post_content['title'] = $this->post['title'];
+      $this->post_content['content'] = $this->post['content'];
+    }
+
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function createMethod()
+    {
+        $title   = $this->post['title'];
+        $content = $this->post['content'];
+        if (empty($title && $content)) {
+            $this->redirect('admin');
+        }
+        $createdPost = ModelFactory::getModel('Blog')->createIt($title,$content);
+        $this->redirect('admin', ['createdPost' => $createdPost]);
+    }
+
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function deleteMethod()
+    {
+      $id_post = $this->get['id'];
+
+      $post_confirmed = ModelFactory::getModel('Comments')->listData($id_post, 'post_id');
+
+      if (!empty($post_confirmed))
+      {
+        ModelFactory::getModel('Comments')->deleteData($id_post, 'post_id');
+      }
+
+      ModelFactory::getModel('Blog')->deleteData($id_post);
+
+      $this->redirect('admin');
+    }
+
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function modifyMethod()
+    {
+      if (!empty($this->post)) {
+        $this->postData();
+
+        ModelFactory::getModel('Blog')->modifyIt($this->get['id'], $this->post_content['title'],$this->post_content['content']);
+
+        $this->redirect('admin');
+    }
+    $Blog = ModelFactory::getModel('Blog')->readData($this->get['id']);
+    $comments = ModelFactory::getModel('Comments')->listData($this->get['id'], 'post_id');
+
+      return $this->render('backend/modifyBlog.twig', [
+        'Blog' => $Blog,
+        'comments' => $comments
+      ]);
     }
 }
