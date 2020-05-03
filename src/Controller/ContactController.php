453ae20ec85ebdd5ad127584cea5fd13f6b3cacd
Matthias <m.geslin974@gmail.com>
2020-05-04 00:43:15 +0200

    contact controller updated w/ mail method

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -24,4 +24,37 @@ class ContactController extends MainController
     {
         return $this->render('contact.twig');
     }
+
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function mailMethod()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST')
+        {
+            $firstname    = htmlentities($this->post['first_name']);
+            $lastname     = htmlentities($this->post['last_name']);
+            $email        = htmlentities($this->post['email']);
+            $subjectPost  = htmlentities($this->post['subject']);
+            $content      = $this->post['content'];
+
+            $from    = $email;
+            $to      = "m.geslin974@gmail.com";
+
+            $subject =  'Message de ' .$firstname.' <'.$email.'>';
+            $message = $content;
+
+            $header  = 'MIME-Version: 1.0'."\r\n";
+            $header .= 'Content-type: text/html; charset=utf-8'."\r\n";
+            $header .= 'From: '.$from."\r\n";
+
+            mail($to,$subject,$message, $header);
+
+            $this->redirect('contact');
+        }
+        return $this->render('contact.twig');
+    }
 }
