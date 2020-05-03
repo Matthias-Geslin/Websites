3ef6e7653e39e0cd077224bb9ad16d6965a1b8c3
Matthias <m.geslin974@gmail.com>
2020-05-04 00:41:47 +0200

    create/modify methods
    for future use of Tinymce

--- src/Model/BlogModel.php
+++ src/Model/BlogModel.php
@@ -1,8 +1,34 @@
 <?php
 namespace App\Model;
 
+use App\Model\Maker\PDOMaker;
 /**
  * Class BlogModel
  * @package App\Model
  */
-class BlogModel extends MainModel{}
+class BlogModel extends MainModel
+{
+
+    /**
+    * @param string $title
+    * @param string $content
+    * @return bool
+    */
+    public function createIt(string $title, string $content)
+    {
+        $req = PDOMaker::getPDO()->prepare('INSERT INTO Posts(title,content) VALUES(?,?)');
+        return $req->execute(array($title,$content));
+    }
+
+    /**
+     * @param int $id
+     * @param string $title
+     * @param string $content
+     */
+    public function modifyIt(int $id, string $title ,string $content)
+    {
+        $content = html_entity_decode($content);
+        $req = PDOMaker::getPDO()->prepare('UPDATE Posts SET title = ? , content = ? WHERE id = ?');
+        $req->execute(array($title,$content,$id));
+    }
+}
